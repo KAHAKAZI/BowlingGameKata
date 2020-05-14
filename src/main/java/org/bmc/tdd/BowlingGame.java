c6d8f57e6d3cc0fe884303b806fcbfa565769fec
kahakazi <kahakazi@gmail.com>
2020-05-14 18:53:56 +0200

    it #3

--- src/main/java/org/bmc/tdd/BowlingGame.java
+++ src/main/java/org/bmc/tdd/BowlingGame.java
@@ -11,4 +11,23 @@ public class BowlingGame {
         return "X".equals(input);
     }
 
+    public int getScoreForNow(String[] args, int index) {
+
+        if ("X".equals(args[index])) {
+            int value = getAsInt(args[index]) +
+                    getAsInt(args[index + 1]) +
+                    getAsInt(args[index + 2]);
+            return value;
+        }
+
+        return -1;
+    }
+
+    public int getAsInt(String element){
+        if ("X".equals(element)){
+            return 10;
+        }
+        return Integer.parseInt(element);
+    }
+
 }
--- src/test/java/org/bmc/tdd/BowlingGameTest.java
+++ src/test/java/org/bmc/tdd/BowlingGameTest.java
@@ -19,4 +19,25 @@ class BowlingGameTest {
     public void shouldBeStrike(){
         Assertions.assertThat(theGame.isStrike("X")).isEqualTo(true);
     }
+
+    @Test
+    public void shouldReturn30For3Strikes(){
+        String[] values = {"X", "X", "X"};
+        int index = 0;
+        Assertions.assertThat(theGame.getScoreForNow(values, index)).isEqualTo(30);
+    }
+
+    @Test
+    public void shouldReturn2StrikesPlus3Rd(){
+        String[] values = {"X", "X", "3", "4"};
+        int index = 0;
+        Assertions.assertThat(theGame.getScoreForNow(values, index)).isEqualTo(23);
+    }
+
+    @Test
+    public void shouldReturn1StrikePlus2Next(){
+        String[] values = {"X", "2", "3"};
+        int index = 0;
+        Assertions.assertThat(theGame.getScoreForNow(values, index)).isEqualTo(15);
+    }
 }
